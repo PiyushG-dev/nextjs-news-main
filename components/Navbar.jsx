f2b2aff8716a74bcd8f57dc98bd70edf75e567b5
Piyush Goswami <pjgpiyush@gmail.com>
2024-04-22 23:36:56 +0530

    login and signup pages added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./ModeToggle";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="w-full flex items-center justify-center">
       <nav className="w-[1120px] flex justify-between items-center py-4 lg:px-5">
-        <h1>Indie News</h1>
+        <Link href="/">Indie News</Link>
         <ul className="flex gap-5 items-center">
-          <li>Headlines</li>
+          <Link
+            href="/"
+            className="hover:text-muted-foreground transition-colors"
+          >
+            Headlines
+          </Link>
           <li>Innovation</li>
           <li>Politics</li>
           <li>Technology</li>
           <li>Startups</li>
-          <Button>Login</Button>
+          <Button>
+            <Link href="/login">Login</Link>
+          </Button>
           <ModeToggle />
         </ul>
       </nav>
